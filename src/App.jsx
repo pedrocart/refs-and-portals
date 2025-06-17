9b88c829f7f104a69af396671a11408c68b45150
Pedro Costa <pedrocart@gmail.com>
2025-06-17 20:10:33 -0300

    Timer Functioning indenpently

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import Player from './components/Player.jsx';
+import TimerChalenge from './components/TimerChallenge.jsx';
 
 function App() {
   return (
     <>
       <Player />
-      <div id="challenges"></div>
+      <div id="challenges">
+         <TimerChalenge title="Easy" targetTime={1}/>
+         <TimerChalenge title="Not Easy" targetTime={5}/>
+         <TimerChalenge title="Getting Tough" targetTime={10}/>
+         <TimerChalenge title="Pros Only" targetTime={15}/>
+      </div>
     </>
   );
 }
