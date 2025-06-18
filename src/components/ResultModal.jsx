0f04cb5bfdc15f3069396a677153d8f0a271e201
Pedro Costa <pedrocart@gmail.com>
2025-06-17 21:00:02 -0300

    Using ImperativeHandle to expose callable functions from inside your components

--- src/components/ResultModal.jsx
+++ src/components/ResultModal.jsx
@@ -1,11 +1,21 @@
-import { forwardRef } from 'react';
+import { forwardRef, useImperativeHandle, useRef } from 'react';
 
 const ResultModal = forwardRef(function ResultModal({ result, targetTime }, ref) {
+   const dialog = useRef();
+
+   useImperativeHandle(ref, () => {
+      return {
+         open() {
+            dialog.current.showModal();
+         }
+      };
+   });
+
    return (
-      <dialog ref={ref} className="result-modal">
+      <dialog ref={dialog} className="result-modal">
          <h2>You {result ? 'won' : 'lost'}</h2>
          <p>
-            The target time was <storng>{targetTime} seconds.</storng>
+            The target time was <strong>{targetTime} seconds.</strong>
          </p>
          <p>
             You stopped the timer with <strong>X seconds left.</strong>
